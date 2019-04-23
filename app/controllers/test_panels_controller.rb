726c019c985f9146e26cad00701c6b4b4040ed2f
ttam <mattgooglin@gmail.com>
2019-04-23 10:06:13 -0400

    Refactors code by adding message for when no test panel is found & moves logic in controller into methods

--- app/controllers/test_panels_controller.rb
+++ app/controllers/test_panels_controller.rb
@@ -1,16 +1,33 @@
 class TestPanelsController < ApplicationController
 
   def show
-    test_panel_id = params[:id]
-    test_panel = TestPanel::DATA.find { |panel| panel[:id] == test_panel_id }
 
-    #gather tests
+    test_panel_id = params[:id]
+    test_panel = TestPanel::DATA.find { |panel| panel[:id] == test_panel_id.upcase }
+
+    if test_panel.nil?
+      render json: {
+          message: 'No test panels were found',
+          status: 404
+      }
+    else
+      @my_panel = {}
+      my_tests = gather_tests(test_panel)
+      setup_test_panel_data(test_panel, my_tests)
+      render '/test_panels'
+    end
+
+  end
+
+  def gather_tests(test_panel)
     my_tests = []
     test_panel[:tests].each do |test|
       my_tests.push Test::DATA.find { |item| item[:id] == test}
     end
+    my_tests
+  end
 
-    @my_panel = {}
+  def setup_test_panel_data(test_panel, my_tests)
     @my_panel[:id] = test_panel[:id]
     @my_panel[:price] = test_panel[:price]
     @my_panel[:sample_tube_volumes] = my_tests.map { |item| item[:sample_tube_type].to_s  }
@@ -27,15 +44,7 @@ class TestPanelsController < ApplicationController
     end
 
     @my_panel[:included] = my_tests
-
-
-
-
-
-    render '/test_panels'
-
   end
-
 
 
 
